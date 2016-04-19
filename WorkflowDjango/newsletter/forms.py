729dd37378654c9248dd006132fd2042f39bb072
sanket2407 <sanket24.07@gmail.com>
2016-04-18 18:16:29 -0700

    add: admin form template

---
+++ WorkflowDjango/newsletter/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+form .models import SignUp
+
+class SignUpForm(forms.ModelForm):
+	class Meta:
+		model = SignUp
+		fields = ['full_name', 'email']
+		#exclude = ['full_name']
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		print email
+		email_base, provider = email.split("@")
+		domain, extension = provider.split('.')
+		if not extension == "edu":
+			raise forms.ValidationError("Enter valid email having .edu extension")
+		return email
