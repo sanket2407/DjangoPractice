15f31c287ca14aed341a3632f9544140b2bf43b2
sanket2407 <sanket24.07@gmail.com>
2016-04-19 19:48:07 -0700

    add: custom form instead of direct model into admin

--- WorkflowDjango/feedback/admin.py
+++ WorkflowDjango/feedback/admin.py
@@ -2,10 +2,24 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import FeedBack
+from .forms import FeedBackForm
 
 class FeedBackAdmin(admin.ModelAdmin):
+
+# defining format to record detail
+
 	list_display = ["__unicode__", "feedback"]
-	class Meta:
-		model = FeedBack
+
+# adding model directly to the admin
+
+	# class Meta:
+	# 	model = FeedBack
+
+# we are going to add form here instead of direct model
+	form = FeedBackForm
+
 
 admin.site.register(FeedBack, FeedBackAdmin)
+
+
+
---
+++ WorkflowDjango/feedback/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import FeedBack
+
+class FeedBackForm(forms.ModelForm):
+	
+	class Meta:
+		model = FeedBack
+		fields = ['email']
