63f91d7591e11cddfe3f26c431f20e13c1db7fb8
sanket2407 <sanket24.07@gmail.com>
2016-04-19 19:12:30 -0700

    add: feedback form in admin

--- WorkflowDjango/feedback/admin.py
+++ WorkflowDjango/feedback/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import FeedBack
+
+class FeedBackAdmin(admin.ModelAdmin):
+	list_display = ["__unicode__", "feedback"]
+	class Meta:
+		model = FeedBack
+
+admin.site.register(FeedBack, FeedBackAdmin)
